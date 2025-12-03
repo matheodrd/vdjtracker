633994b94af676148b75d9db862d9ccdfbb96ae6
matheodrd <matheodurand14@gmail.com>
2025-12-03 15:34:25 +0100

    refactor: move file Watcher into file package

--- main.go
+++ main.go
@@ -2,65 +2,13 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
 	"time"
+
+	"github.com/matheodrd/vdjtracker/internal/file"
 )
 
-type Watcher struct {
-	FilePath    string
-	PollingRate time.Duration
-}
-
-func NewWatcher(filePath string, pollingRate time.Duration) *Watcher {
-	return &Watcher{FilePath: filePath, PollingRate: pollingRate}
-}
-
-// Watch watches a file and send its content in a channel when a change is detected.
-// Content is space-trimmed.
-func (w *Watcher) Watch() <-chan string {
-	ch := make(chan string)
-
-	go func() {
-		defer close(ch)
-
-		var lastModTime time.Time
-		var lastContent string
-
-		ticker := time.NewTicker(w.PollingRate)
-		defer ticker.Stop()
-
-		for range ticker.C {
-			info, err := os.Stat(w.FilePath)
-			if err != nil {
-				continue // file not yet created
-			}
-
-			// check if file has been modified
-			if !info.ModTime().After(lastModTime) {
-				continue
-			}
-			lastModTime = info.ModTime()
-
-			data, err := os.ReadFile(w.FilePath)
-			if err != nil {
-				continue
-			}
-
-			content := strings.TrimSpace(string(data))
-
-			if content != "" && content != lastContent {
-				lastContent = content
-				ch <- content
-			}
-		}
-	}()
-
-	return ch
-}
-
 func main() {
-	watcher := NewWatcher("test.txt", 500*time.Millisecond)
+	watcher := file.NewWatcher("test.txt", 500*time.Millisecond)
 
 	tracks := watcher.Watch()
 	for track := range tracks {
